8e5d6517107d756a9a61bed3729b0132bdd2b793
Abhishek singh <abhisheksingh6716@gmail.com>
2021-07-23 23:46:03 +0530

    implementing other components

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../Store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const noOfCartItems = cartCtx.items.reduce((currNo, item) => {
+    return currNo + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{noOfCartItems}</span>
     </button>
   );
 };
